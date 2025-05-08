0854ac222e2ee1c7a85b2e1c2d9eeb506bc4efe7
PRATANGGA <pratanggajulang31@gmail.com>
2025-05-08 14:33:18 +0000

    install.sh

--- install.sh
+++ install.sh
@@ -8,7 +8,8 @@ php artisan key:generate
 
 sed -i 's/DB_HOST=127.0.0.1/DB_HOST=172.17.0.2/g' .env &&
 sed -i 's/DB_PASSWORD=/DB_PASSWORD=password/g' .env &&
-
+php artisan clear-compiled 
+composer update
 php artisan migrate
 php artisan db:seed
 php artisan storage:link
